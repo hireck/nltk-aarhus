ccf497ee3d273792d5a50fe35b2ce5718ce546de
hireck <reckmanhgb@gmail.com>
2014-10-11 11:28:27 +0200

    Update ch4ex32_summarize_a.py

--- ch4ex32_summarize_a.py
+++ ch4ex32_summarize_a.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python 
 # -*- coding: utf-8 -*-
 
-'''Develop a simple extractive summarization tool, that prints the sentences of a document which contain the highest total word frequency. Use FreqDist() to count word frequencies, and use sum to sum the frequencies of the words in each sentence. Rank the sentences according to their score. Finally, print the n highest-scoring sentences in document order. Carefully review the design of your program, especially your approach to this double sorting. Make sure the program is written as clearly as possible. '''
+'''Develop a simple extractive summarization tool, that prints the sentences of a document which contain 
+the highest total word frequency. Use FreqDist() to count word frequencies, 
+and use sum to sum the frequencies of the words in each sentence. 
+Rank the sentences according to their score. 
+Finally, print the n highest-scoring sentences in document order. 
+Carefully review the design of your program, especially your approach to this double sorting. 
+Make sure the program is written as clearly as possible. '''
 
 from __future__ import division
 import nltk
